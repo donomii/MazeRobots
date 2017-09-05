eda73c2e45a3232446b3c4ae467ed7479e37fcc5
Donomii <donomii@gmail.com>
2017-09-05 17:49:56 +0200

    Remove comments, add tests, make closed set lexically scoped

--- mystar.rkt
+++ mystar.rkt
@@ -11,6 +11,7 @@
 (require (prefix-in arr: math/matrix))
 (require math/array)
 [require srfi/1]
+[require rackunit]
 (define (test-map-1)
   (array-map (lambda (x) [if [> x 1]
                              9001
@@ -74,8 +75,6 @@
                                ])]
 
 
-[define closed [make-hash]]
-
 [define [moveTo a b c]
   ;[printf "Moving from ~a to ~a, step ~a~n" a b c]
   [if  [< [* [- a b] [- a b]] c]
@@ -130,19 +129,19 @@
 [define [neighbour-list] '[(-1 0) (1 0) (0 -1) (0 1) ]]
 
 ;[improvePath [make-map] [basicLine start end]]
-[neighbour-list]
+
 
 [define [generate-new-paths path neighbours]
   [map [lambda [n]
          [cons [map + n [car path]] path]]
        neighbours]]
 
-[define [doThing smap path start return]
+[define [doThing smap path start return closed]
   [hash-set! closed [car path] 1]
   ;[showmap smap path closed][displayln ""]
   
   ;[printf "Score: ~a, path: ~a~n"  [lineScore smap path start] path]
-  [if [equal? [car path] start]
+  [if [equal? [car path] start ]
       [return path]
       [let [[new-paths [filter [lambda [e]
                                  ;[printf "Position (~a,~a)~n"[caar e] [second [first e]]]
@@ -159,7 +158,7 @@
    
         [if [empty? new-paths]
             '[]
-            [map [lambda [a-path] [doThing smap a-path start return]] [sort  new-paths < #:key [lambda [l] [+ [lineScore smap l start] [/ [mapScore smap l start] 1000000]]]]]
+            [map [lambda [a-path] [doThing smap a-path start return closed]] [sort  new-paths < #:key [lambda [l] [+ [lineScore smap l start] [/ [mapScore smap l start] 1000000]]]]]
             ]]]]
 
 
@@ -192,18 +191,81 @@
     ]]
 
 [define [find-path smap start end]
-  [printf "Navigating matrix of size ~ax~a~n" [arr:matrix-num-cols smap] [arr:matrix-num-rows smap]]
-  [set! closed [make-hash]]
+  ;[printf "Navigating matrix of size ~ax~a~n" [arr:matrix-num-cols smap] [arr:matrix-num-rows smap]]
+  [let [[closed [make-hash]]]
   [letrec [
-           [path [call/cc [lambda [return] [doThing smap [list start] end return]]]]]
-    [displayln "calculated path"]
+           [path [call/cc [lambda [return] [doThing smap [list start] end return closed]]]]]
+    ;[displayln "calculated path"]
     
-    [showmap smap path closed]
-    [not [empty? path]]]]
+    ;[showmap smap path closed]
+    ;[not [empty? path]]
+    path]]]
 
-hash-map
 
-;[find-path [array->mutable-array [test-map-2]] '[4 3] '[0 0]]
+
+[check-equal? [find-path [array->mutable-array [test-map-2]] '[4 3] '[0 0]] '((0 0) (0 1) (0 2) (0 3) (1 3) (2 3) (2 2) (2 1) (2 0) (3 0) (4 0) (4 1) (4 2) (4 3)) "Check a simple map"]
 ;[find-path [array->mutable-array [test-map-1]] '[7 7] '[1 1]]
 ;[find-path [array->mutable-array [test-map-3]] '[19 19] '[0 0]]
-;[find-path [array->mutable-array [test-map-4]] '[19 19] '[0 0]]
+[check-equal? [find-path [array->mutable-array [test-map-4]] '[19 19] '[0 0]] '((0 0)
+  (1 0)
+  (1 1)
+  (1 2)
+  (2 2)
+  (3 2)
+  (4 2)
+  (5 2)
+  (6 2)
+  (7 2)
+  (7 3)
+  (7 4)
+  (8 4)
+  (9 4)
+  (9 3)
+  (9 2)
+  (10 2)
+  (11 2)
+  (11 3)
+  (11 4)
+  (11 5)
+  (11 6)
+  (11 7)
+  (11 8)
+  (12 8)
+  (13 8)
+  (13 7)
+  (13 6)
+  (14 6)
+  (15 6)
+  (16 6)
+  (17 6)
+  (18 6)
+  (19 6)
+  (19 7)
+  (19 8)
+  (18 8)
+  (17 8)
+  (16 8)
+  (15 8)
+  (15 9)
+  (15 10)
+  (16 10)
+  (17 10)
+  (17 11)
+  (17 12)
+  (18 12)
+  (19 12)
+  (19 13)
+  (19 14)
+  (18 14)
+  (17 14)
+  (16 14)
+  (15 14)
+  (15 15)
+  (15 16)
+  (16 16)
+  (17 16)
+  (18 16)
+  (19 16)
+  (19 17)
+  (19 18)
+  (19 19)) "Check a big maze"]
